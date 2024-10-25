1d4e4aa76799bc8a74177473a2caa9f7a3eb5937
Toap777 <toap.gester@outlook.de>
2024-10-25 18:14:56 +0200

    build scaffold for  form code.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,197 @@
 <script lang="ts">
 	import Card from '$lib/Card.svelte';
 	import type { PageData } from './$types';
+	import { Button, Modal, Label, Input, Checkbox } from 'flowbite-svelte';
 
 	let { data } = $props();
 	let eventIdeas = $state(data.eventIdeas);
+	let formModal = $state(true);
+
 	// export let data: PageData;
 	// $: eventIdeas = data.eventIdeas;
+
+	interface FormField {
+		name: string;
+		value: string;
+		required: boolean;
+	}
+
+	function submitForm(event: Event) {
+		event.preventDefault(); // Prevent the default form submission
+		let formIsValid = true;
+
+		const formData: { [key: string]: string } = {};
+
+		const fields: FormField[] = [
+			{
+				name: 'iconSelect',
+				value: (document.querySelector('input[name="iconSelect"]:checked') as HTMLInputElement)
+					.value,
+				required: true
+			},
+			{
+				name: 'title',
+				value: (document.querySelector('input[name="title"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'details',
+				value: (document.querySelector('input[name="details"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'startDate',
+				value: (document.querySelector('input[name="startDate"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'startTime',
+				value: (document.querySelector('input[name="startTime"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'endDate',
+				value: (document.querySelector('input[name="endDate"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'endTime',
+				value: (document.querySelector('input[name="endTime"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'price',
+				value: (document.querySelector('input[name="price"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'location',
+				value: (document.querySelector('input[name="location"]') as HTMLInputElement).value,
+				required: true
+			},
+			{
+				name: 'visitorAmount',
+				value: (document.querySelector('input[name="visitorAmount"]') as HTMLInputElement).value,
+				required: true
+			}
+		];
+
+		for (const field of fields) {
+			if (field.required && !field.value) {
+				formIsValid = false;
+				break;
+			}
+			formData[field.name] = field.value;
+		}
+
+		if (formIsValid) {
+			// Close the modal
+			formModal = false;
+
+			data.eventIdeas.push({
+				id: '3',
+				title: formData['title'],
+				description: formData['details'],
+				icon: formData['icons'],
+				likes: 0,
+				location: [0, 0],
+				date: new Date(2022, 5, 12, 14, 30, 0),
+				visitorAmount: Number(formData['visitorAmount']),
+				priceCents: Number(formData['visitorAmount']),
+				creator: 'user'
+			});
+			// Storing data in a global variable
+			//(window as any).globalFormData = formData;
+			//console.log('Form data saved:', formData);
+		} else {
+			//console.log('Please fill out all required fields.');
+		}
+	}
+
+	function closeForm() {
+		formModal = false;
+	}
 </script>
 
 {#each eventIdeas as idea}
 	<Card {idea} />
 {/each}
+
+<Button class="fixed bottom-0 z-1 w-full h-14 text-2xl" on:click={() => (formModal = false)}
+	>Idee einreichen</Button
+>
+
+<Modal bind:open={formModal} size="xs" autoclose={true} class="w-full">
+	<form class="flex flex-col space-y-6 h-160" action="#">
+		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Idee einreichen</h3>
+
+		<Label class="space-y-1">
+			<span>Icon</span>
+			<fieldset class="flex gap-2">
+				<label for="trophyRadioButton">
+					<input type="radio" id="trophyRadioButton" name="iconSelect" value="Trophy" checked />
+					Trophy
+				</label>
+
+				<label for="ballRadioBtn">
+					<input type="radio" id="ballRadioBtn" name="iconSelect" value="Ball" />
+					Ball
+				</label>
+			</fieldset>
+		</Label>
+
+		<Label class="space-y-1">
+			<span>Thema</span>
+			<Input type="text" name="title" placeholder="Your Event" required />
+		</Label>
+
+		<Label class="space-y-2">
+			<span>Details</span>
+			<Input type="text" name="details" placeholder="Lets meet up together !" required />
+		</Label>
+
+		<Label>
+			<span>Zeitraum wählen</span>
+			<fieldset>
+				<Label class="space-y-2">
+					<span>Von</span>
+					<Input type="date" name="startDate" required />
+					<Input type="time" name="startTime" required />
+				</Label>
+
+				<Label class="space-y-2">
+					<span>Bis</span>
+					<Input type="date" name="endDate" required />
+					<Input type="time" name="endTime" required />
+				</Label>
+			</fieldset>
+		</Label>
+
+		<Label class="space-y-2">
+			<span>Preis in €</span>
+			<Input type="number" name="price" placeholder="10" min="1" max="1000" step="any" required />
+		</Label>
+
+		<Label class="space-y-2">
+			<span>Ort</span>
+			<Input type="text" name="location" placeholder="Zwickau" required />
+		</Label>
+
+		<Label class="space-y-2">
+			<span>Besucherzahl</span>
+			<Input
+				type="number"
+				name="visitorAmount"
+				placeholder="5"
+				min="1"
+				max="1000"
+				step="any"
+				required
+			/>
+		</Label>
+		<div class="flex row gap-5 w-full justify-evenly">
+			<Button on:click={closeForm} color="alternative" class="w-2/5 inline-block">Zurück</Button>
+			<Button on:click={submitForm} type="submit" class="w-2/5 inline-block">Absenden</Button>
+		</div>
+	</form>
+</Modal>
