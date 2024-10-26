cc22cb33d37ea3a1daa4db111c90266600465d44
Toap777 <toap.gester@outlook.de>
2024-10-26 06:18:34 +0200

    Refactored form submit to use POST instead of JS now.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,9 @@
 import { eventIdeas } from '$lib/data';
 import { db } from '$lib/server/db';
+import type { EventIdeaTableEntry } from '$lib/server/db/schema';
 import type { EventIdea } from '$lib/types';
 import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({
 	url,
@@ -11,4 +13,86 @@ export const load: PageServerLoad = async ({
 	return {
 		eventIdeas: new Array(1).fill(eventIdeas).flat()
 	};
+};
+
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+
+		console.log(data);
+
+		const icon = data.get('icon');
+		if (!icon) {
+			return fail(400, { icon, missing: true });
+		}
+
+		const title = data.get('title');
+		if (!title) {
+			return fail(400, { title, missing: true });
+		}
+
+		const details = data.get('details');
+		if (!details) {
+			return fail(400, { details, missing: true });
+		}
+
+		const startDate = data.get('startDate');
+		if (!startDate) {
+			return fail(400, { startDate, missing: true });
+		}
+
+		const startTime = data.get('startTime');
+		if (!startDate) {
+			return fail(400, { startTime, missing: true });
+		}
+
+		const endDate = data.get('endDate');
+		if (!startDate) {
+			return fail(400, { endDate, missing: true });
+		}
+
+		const price = data.get('price');
+		if (!price) {
+			return fail(400, { endDate, missing: true });
+		}
+
+		const town = data.get('town');
+		if (!town) {
+			return fail(400, { town, missing: true });
+		}
+
+		const latitude = data.get('latitude');
+		if (!latitude) {
+			return fail(400, { latitude, missing: true });
+		}
+
+		const longitude = data.get('longitude');
+		if (!longitude) {
+			return fail(400, { longitude, missing: true });
+		}
+
+		const visitorAmount = data.get('visitorAmount');
+		if (!visitorAmount) {
+			return fail(400, { visitorAmount, missing: true });
+		}
+
+		console.log('Alles eingelesen.');
+
+		let newEventIdea: EventIdea = {
+			id: '3',
+			title: title.toString(),
+			description: details.toString(),
+			icon: icon.toString(),
+			likes: 0,
+			location: [Number(longitude), Number(latitude)],
+			townPrecomputed: town.toString(),
+			date: new Date(2022, 5, 12, 14, 30, 0),
+			visitorAmount: Number(visitorAmount),
+			priceCents: Number(price),
+			creator: 'user'
+		};
+
+		eventIdeas.push(newEventIdea);
+		return { success: true };
+	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,110 +11,6 @@
 	let eventIdeas = $state(data.eventIdeas);
 	let formModal = $state(false);
 
-	interface FormField {
-		name: string;
-		value: string;
-		required: boolean;
-	}
-
-	async function submitForm(event: Event) {
-		event.preventDefault(); // Prevent the default form submission
-		let formIsValid = true;
-
-		const formData: { [key: string]: string } = {};
-
-		console.log(document.querySelector('input[name="iconSelect"]:checked') as HTMLInputElement);
-		const fields: FormField[] = [
-			{
-				name: 'icon',
-				value: (document.querySelector('input[name="iconSelect"]:checked') as HTMLInputElement)
-					.value,
-				required: true
-			},
-			{
-				name: 'title',
-				value: (document.querySelector('input[name="title"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'details',
-				value: (document.querySelector('textarea[name="details"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'startDate',
-				value: (document.querySelector('input[name="startDate"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'startTime',
-				value: (document.querySelector('input[name="startTime"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'endDate',
-				value: (document.querySelector('input[name="endDate"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'endTime',
-				value: (document.querySelector('input[name="endTime"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'price',
-				value: (document.querySelector('input[name="price"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'location',
-				value: (document.querySelector('input[name="location"]') as HTMLInputElement).value,
-				required: true
-			},
-			{
-				name: 'visitorAmount',
-				value: (document.querySelector('input[name="visitorAmount"]') as HTMLInputElement).value,
-				required: true
-			}
-		];
-
-		for (const field of fields) {
-			console.dir(field);
-			if (field.required && !field.value) {
-				formIsValid = false;
-				break;
-			}
-			formData[field.name] = field.value;
-		}
-
-		if (formIsValid) {
-			// Close the modal
-			formModal = false;
-
-			let newEventIdea = {
-				id: '3',
-				title: formData['title'],
-				description: formData['details'],
-				icon: formData['icon'],
-				likes: 0,
-				location: location!.lngLat,
-				townPrecomputed: location!.town,
-				date: new Date(2022, 5, 12, 14, 30, 0),
-				visitorAmount: Number(formData['visitorAmount']),
-				priceCents: Number(formData['visitorAmount']),
-				creator: 'user'
-			};
-			eventIdeas.push(newEventIdea);
-			// TODO: call action in +page.server.ts
-			const response = await fetch('/', {
-				method: 'POST',
-				body: JSON.stringify(newEventIdea)
-			});
-		} else {
-			console.log('Please fill out all required fields.');
-		}
-	}
-
 	function closeForm() {
 		formModal = false;
 	}
@@ -136,19 +32,19 @@
 >
 
 <Modal bind:open={formModal} size="xs" class="w-full">
-	<form class="flex flex-col space-y-6 h-160" action="#">
+	<form class="flex flex-col space-y-6 h-160" action="#" method="POST">
 		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Idee einreichen</h3>
 
 		<Label class="space-y-1">
 			<span>Icon</span>
 			<fieldset class="flex gap-2">
 				<label for="trophyRadioButton">
-					<input type="radio" id="trophyRadioButton" name="iconSelect" value="trophy" checked />
+					<input type="radio" id="trophyRadioButton" name="icon" value="trophy" checked />
 					Trophy
 				</label>
 
 				<label for="ballRadioBtn">
-					<input type="radio" id="ballRadioBtn" name="iconSelect" value="volleyball" />
+					<input type="radio" id="ballRadioBtn" name="icon" value="volleyball" />
 					Ball
 				</label>
 			</fieldset>
@@ -188,19 +84,14 @@
 
 		<Label class="space-y-2">
 			<span>Ort</span>
-			<Input
-				type="text"
-				name="location"
-				readonly
-				required
-				on:click={openMap}
-				value={location?.town}
-			>
+			<Input type="text" name="town" readonly required on:click={openMap} value={location?.town}>
 				<svelte:fragment slot="left">
 					<!-- <Locate /> -->
 					<MapPin />
 				</svelte:fragment>
 			</Input>
+			<Input type="hidden" name="latitude" value={location?.lngLat.lat}></Input>
+			<Input type="hidden" name="longitude" value={location?.lngLat.lng}></Input>
 		</Label>
 
 		<Label class="space-y-2">
@@ -217,7 +108,7 @@
 		</Label>
 		<div class="flex row gap-5 w-full justify-evenly">
 			<Button on:click={closeForm} color="alternative" class="w-2/5 inline-block">Zurück</Button>
-			<Button on:click={submitForm} type="submit" class="w-2/5 inline-block">Absenden</Button>
+			<Button type="submit" class="w-2/5 inline-block">Absenden</Button>
 		</div>
 	</form>
 </Modal>
@@ -238,7 +129,7 @@
 				mapModal = false;
 				// the hackiest shit code I ever wrote probably
 				await tick();
-				(document.querySelector('input[name="location"]') as HTMLInputElement).scrollIntoView();
+				(document.querySelector('input[name="town"]') as HTMLInputElement).scrollIntoView();
 			}}
 		/>
 	</Portal>
